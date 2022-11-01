fcebce0d7477becd1dd127d13633dcda0d38223a
mrunlockedtech <jrbhrn@gmail.com>
2022-11-01 13:58:08 -0500

    Complete Exercise 2

--- python_functions.py
+++ python_functions.py
@@ -1,7 +1,17 @@
-def sum_to(n):
-  sum = 0
-  for num in range(1,n+1):
-    sum += num
-  return sum
+# Exercise 1
 
-print(sum_to(10))
+# def sum_to(n):
+#   sum = 0
+#   for num in range(1,n+1):
+#     sum += num
+#   return sum
+
+# print(sum_to(10))
+
+# Exercise 2
+
+def largest(nums):
+  nums.sort()
+  return nums[-1]
+print(largest([1,2,3,4,0]))
+print(largest([10,4,2,231,91,54]))
