068d2c4f42c9c36ceb4dfaba40ec144f6f353c65
mrunlockedtech <jrbhrn@gmail.com>
2022-11-01 14:19:36 -0500

    Complete Exercise 3

--- python_functions.py
+++ python_functions.py
@@ -1,3 +1,4 @@
+import re
 # Exercise 1
 
 # def sum_to(n):
@@ -10,8 +11,18 @@
 
 # Exercise 2
 
-def largest(nums):
-  nums.sort()
-  return nums[-1]
-print(largest([1,2,3,4,0]))
-print(largest([10,4,2,231,91,54]))
+# def largest(nums):
+#   nums.sort()
+#   return nums[-1]
+# print(largest([1,2,3,4,0]))
+# print(largest([10,4,2,231,91,54]))
+
+# Exercise 3
+
+def occurrences(total_string, sub_string):
+  return len(re.findall(sub_string,total_string))
+  
+print(occurrences('fleep floop', 'e'))
+print(occurrences('fleep floop', 'p'))
+print(occurrences('fleep floop', 'ee'))
+print(occurrences('fleep floop', 'fe'))
